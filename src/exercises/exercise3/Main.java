3ac14e29a412292ef4e327feaab8a6cff9c16f7f
pask517 <andreapascarella99@gmail.com>
2026-01-13 09:24:02 +0100

    updated

--- src/exercises/exercise3/Main.java
+++ src/exercises/exercise3/Main.java
@@ -1,8 +1,15 @@
 package exercises.exercise3;
 
+import exercises.BancaException;
+
 public class Main {
     public static void main(String[] args) {
         ContoCorrente contoCorrente = new ContoCorrente("Giuseppe", 100);
         ContoOnline contoOnline = new ContoOnline("Andrea", 100, 80);
+        try {
+            contoCorrente.preleva(120);
+        } catch (BancaException e) {
+            System.out.println(e);
+        }
     }
 }
